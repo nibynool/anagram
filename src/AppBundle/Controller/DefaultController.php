134a35265f60bd80cc749968e8d59a4699645733
Michael Lambert <michael@alphageek.com.au>
2018-11-16 19:54:21 +1100

    Configure FOS REST Bundle to detect request type

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,36 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use FOS\RestBundle\Controller\FOSRestController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController extends Controller
+class DefaultController extends FOSRestController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="default")
      */
     public function indexAction(Request $request)
     {
+        $data = null;
+        $view = $this->view($data, 200)
+            ->setTemplate('default/index.html.twig');
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->handleView($view);
+    }
+
+    /**
+     * @Route("/ping", name="ping")
+     */
+    public function pingAction(Request $request)
+    {
+        $data = [
+            'status' => 'success'
+        ];
+
+        $view = $this->view($data, 200)
+            ->setTemplate('default/index.html.twig');
+        // replace this example code with whatever you need
+        return $this->handleView($view);
     }
 }
